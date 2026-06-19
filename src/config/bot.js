90f211a9b1a74c1fa395b67121770b919f375089
Shako508 <grgiswefkyhalem123@gmail.com>
2026-06-19 21:14:49 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Come Playing Minecraft get IP ❤️",
+        name: "Come Playing Minecraft ❤️",
         // Activity type number (0 = Playing).
         type: 0,
       },
