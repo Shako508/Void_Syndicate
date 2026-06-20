b77c96649aac7d5e5906b47e556175403253bbc1
Shako508 <grgiswefkyhalem123@gmail.com>
2026-06-20 22:46:18 +0300

    autoResponses.js

---
+++ src/config/autoResponses.js
@@ -0,0 +1,88 @@
+// config/autoResponses.js
+
+export const KEYWORD_REPLIES = {
+  // عربي
+  'سلام': 'وعليكم السلام 🌸',
+  'السلام': 'وعليكم السلام 🌸',
+  'هلا': 'هلا والله 🌟',
+  'كيفك': 'الحمد لله، وإنت؟ 😊',
+  'شكرا': 'العفو 🙏',
+  'حبيبي': 'حبيبي والله ❤️',
+  'الو': 'أهلاً بك 🙋‍♂️',
+  'صباح الخير': 'صباح النور ☀️',
+  'مساء الخير': 'مساء الفل 🌙',
+  'تصبح على خير': 'تصبح على خير 🌙',
+  'بالتوفيق': 'الله يوفقك يا رب 🤲',
+  'ماشاء الله': 'تبارك الله ❤️',
+  'الحمدلله': 'الحمد لله دائماً وأبداً 🙏',
+  'ان شاء الله': 'إن شاء الله خير 🤲',
+  'ربنا معاك': 'الله معاك يارب 🙏',
+
+  // English
+  'hello': 'Hello there! 👋',
+  'hi': 'Hi! How are you? 😊',
+  'hey': 'Hey! 👋',
+  'how are you': 'I\'m fine, thanks! 😄',
+  'how r u': 'I\'m good! And you? 😊',
+  'thanks': 'You\'re welcome! 🙏',
+  'thank you': 'You\'re welcome! 🙏',
+  'good morning': 'Good morning! ☀️',
+  'good night': 'Good night! 🌙',
+  'good evening': 'Good evening! 🌅',
+  'good afternoon': 'Good afternoon! ☀️',
+  'bye': 'Bye! See you later 👋',
+  'goodbye': 'Goodbye! Take care 👋',
+  'see you': 'See you later! 👋',
+  'nice': 'Nice! 😎',
+  'cool': 'Cool! 😎',
+  'awesome': 'Awesome! 🔥',
+  'amazing': 'Amazing! 🔥',
+  'great': 'Great! 👏',
+  'love': 'Love you too! ❤️',
+  'i love you': 'I love you too! ❤️',
+  'miss you': 'Miss you too! 😢❤️',
+  'happy': 'Happy for you! 😊',
+  'sad': 'Don\'t be sad, I\'m here for you 🤗',
+  'lol': 'LOL! 😂',
+  'haha': 'Haha! 😂',
+  'nice one': 'Nice one! 😎',
+  'good job': 'Good job! 👏',
+  'well done': 'Well done! 👏',
+  'congrats': 'Congratulations! 🎉',
+
+  // Gaming
+  'gg': 'GG! Well played! 🎮',
+  'wp': 'WP! 😎',
+  'ez': 'EZ! 😂',
+  'noob': 'We all start somewhere! 😄',
+  'pro': 'Pro player! 🔥',
+  'ggwp': 'GGWP! 🎮👏',
+
+  // Arabic + English mix
+  'باي': 'باي باي 👋',
+  'سلام': 'سلام عليكم 🌸',
+  'شكرا': 'عفواً 🙏',
+  'أوكي': 'أوكي 👍',
+  'ok': 'Okay! 👍',
+  'okay': 'Okay! 👍',
+
+  // Fun
+  'هههه': 'هههههه 😂',
+  'ههه': 'ههههه 😂',
+  '😂': '😂😂😂',
+  '🤣': '🤣🤣🤣',
+  '❤️': '❤️❤️❤️',
+  '🔥': '🔥🔥🔥',
+
+  // Random questions
+  'عامل ايه': 'الحمد لله، وإنت عامل إيه؟ 😊',
+  'عامل اي': 'الحمد لله، وإنت عامل إيه؟ 😊',
+  'اخبارك': 'الحمد لله، أخبارك إيه؟ ☺️',
+  'اخبارك ايه': 'الحمد لله، أخبارك إيه؟ ☺️',
+  'ايه اخبارك': 'الحمد لله، أخبارك إيه؟ ☺️',
+  'what\'s up': 'Not much! What about you? 😊',
+  'sup': 'Hey! What\'s up? 😄',
+  'wyd': 'Just chatting with you! 😄',
+};
+
+export const AUTO_REPLY_MESSAGE = 'مرحباً بك! أنا بوت تيتان، تحت أمرك 😊🤖';
